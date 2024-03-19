8b359b7890dd9db1d7f11de51082ab94f8fd54f8
kiyomasa05 <m230549@gmail.com>
2024-03-19 13:26:42 +0900

    3_15

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -11,9 +11,10 @@ import {
 } from "@mui/material";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import HomeIcon from '@mui/icons-material/Home';
+import HomeIcon from "@mui/icons-material/Home";
 import EqualizerIcon from "@mui/icons-material/Equalizer";
-import React from "react";
+import React, { CSSProperties } from "react";
+import { NavLink } from "react-router-dom";
 
 interface SidebarProps {
   drawerWidth: number;
@@ -34,11 +35,19 @@ const SideBar = ({
   handleDrawerTransitionEnd,
   handleDrawerClose,
 }: SidebarProps) => {
-
   const MenuItem: menuItem[] = [
     { text: "Home", path: "/", icon: HomeIcon },
     { text: "Report", path: "/report", icon: EqualizerIcon },
   ];
+
+  const baseLinkStyle: CSSProperties = {
+    textDecoration:"none",
+    color: "inherit",
+    display:"block"
+  };
+  const activeLinkStyle: CSSProperties = {
+    backgroundColor: "rgba(0,0,0,0.08)",
+  };
 
   const drawer = (
     <div>
@@ -46,15 +55,23 @@ const SideBar = ({
       <Divider />
       <List>
         {MenuItem.map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                <item.icon />
-              </ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
-          </ListItem>
+          // 選択済みのメニューに対し、背景色を変更させる
+          <NavLink key={item.text} to={item.path} style={({isActive})=>{
+            console.log("選択されたメニューは",item.text,isActive)
+            return {...baseLinkStyle,
+            ...(isActive ? activeLinkStyle:{})
+            }
+          }}>
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                  <item.icon />
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
         ))}
       </List>
       <Divider />
