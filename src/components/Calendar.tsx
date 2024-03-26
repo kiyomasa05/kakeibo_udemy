3b2690b234e517e7dc3316805eed92c05f7ceaca
kiyomasa05 <m230549@gmail.com>
2024-03-26 09:02:05 +0900

    4-28

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import FullCalendar from "@fullcalendar/react";
+import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { EventContentArg } from "@fullcalendar/core";
+import "../calendar.css";
 
 const Calendar = () => {
+  const events = [
+    { title: "Meeting", start: "2024-03-25",income:500,expense:500,balance:200 },
+    { title: "Meeting", start: "2024-03-24",income:500,expense:500,balance:200 },
+  ];
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
   return (
-    <div>
-      カレンダ
-    </div>
-  )
-}
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
+};
 
-export default Calendar
-
+export default Calendar;
