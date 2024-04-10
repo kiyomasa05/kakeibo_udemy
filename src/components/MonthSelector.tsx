3d06c4c1e38b18ada3f99f61d716aa1b9c1df02b
kiyomasa05 <m230549@gmail.com>
2024-04-10 11:03:47 +0900

    9-64

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,26 +1,40 @@
-import { Box, Button } from '@mui/material'
-import React from 'react'
+import { Box, Button } from "@mui/material";
+import React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { ja } from "date-fns/locale";
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+      dateFormats={{ year: "yyyy年", month: "MM月" }}
+    >
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
         <Button color={"error"} variant="contained">
           先月
         </Button>
-        <DatePicker sx={{mx:2}}/>
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2,background:"white" }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          slotProps={{
+            toolbar: {
+              toolbarFormat: "yyyy年MM月",
+            },
+          }}
+        />
         <Button color={"primary"} variant="contained">
           次月
         </Button>
       </Box>
     </LocalizationProvider>
   );
-}
+};
 
-export default MonthSelector
-
+export default MonthSelector;
