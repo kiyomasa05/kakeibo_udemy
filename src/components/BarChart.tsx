2503ab281027b47fb7d8f4e163ef0002c2f56267
kiyomasa05 <m230549@gmail.com>
2024-04-11 10:41:18 +0900

    10-68

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,8 +7,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -21,45 +21,43 @@ ChartJS.register(
 
 const BarChart = () => {
   const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      // legend: {
+      //   position: 'top' as const,
+      // },
+      title: {
+        display: true,
+        text: "日別収支",
+      },
     },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
+  };
   const labels = [
-    '2024-04-10',
-    '2024-04-11',
-    '2024-04-12',
-    '2024-04-13',
-    '2024-04-14',
-    '2024-04-15',
+    "2024-04-10",
+    "2024-04-11",
+    "2024-04-12",
+    "2024-04-13",
+    "2024-04-14",
+    "2024-04-15",
   ];
 
   const data = {
-  labels,
-  datasets: [
-    {
-      label: '支出',
-      data: [100,200,300,400,500,600],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: '収入',
-      data: [100,200,300,400,500,600],
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
+    labels,
+    datasets: [
+      {
+        label: "支出",
+        data: [100, 200, 300, 400, 500, 600],
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "収入",
+        data: [100, 200, 300, 400, 500, 600],
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 };
-  return (
-     <Bar options={options} data={data} />
-  )
-}
 
-export default BarChart
-
+export default BarChart;
