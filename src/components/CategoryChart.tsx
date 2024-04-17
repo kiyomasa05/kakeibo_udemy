c9e440afaf37141f322225f3ce6097b0bb56b33e
kiyomasa05 <m230549@gmail.com>
2024-04-17 11:34:59 +0900

    11-80

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -59,7 +59,10 @@ const CategoryChart = ({
       {} as Record<IncomeCategory | ExpenseCategory, number>
     );
 
-  const categoryLabels = Object.keys(categorySums);
+  const categoryLabels = Object.keys(categorySums) as (
+    | IncomeCategory
+    | ExpenseCategory
+  )[];
   const categoryValues = Object.values(categorySums);
 
   const options = {
@@ -84,27 +87,27 @@ const CategoryChart = ({
     その他: theme.palette.expenseCategoryColor.その他,
   };
 
+  const getCategoryColor = (
+    category: IncomeCategory | ExpenseCategory
+  ): string => {
+    if (selectedType === "income") {
+      return incomeCategoryColor[category as IncomeCategory];
+    } else {
+      return expenseCategoryColor[category as ExpenseCategory];
+    }
+  };
+
   const data: ChartData<"pie"> = {
     labels: categoryLabels,
     datasets: [
       {
         data: categoryValues,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        backgroundColor: categoryLabels.map((category) =>
+          getCategoryColor(category)
+        ),
+        borderColor: categoryLabels.map((category) =>
+          getCategoryColor(category)
+        ),
         borderWidth: 1,
       },
     ],
